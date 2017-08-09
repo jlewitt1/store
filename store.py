d5a6947f8aa5aef2486769847f4c838eb0cec38d
Josh <josh.lewittes@gmail.com>
2017-08-09 13:24:49 +0300

    Update #1
    Setting up initial get functions - displaying products

--- store.py
+++ store.py
@@ -1,13 +1,106 @@
 from bottle import route, run, template, static_file, get, post, delete, request
-from sys import argv
+# from sys import argv
 import json
 import pymysql
 
+# pip install mysql
+
+connection = pymysql.connect(host='sql11.freesqldatabase.com',
+                             user='sql11189253',
+                             password='HttzudApwV',
+                             db='sql11189253',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+cursor = connection.cursor()
+
 @get("/admin")
 def admin_portal():
-	return template("pages/admin.html")
+    return template("pages/admin.html")
 
+# Create a category
+@post("/category")
+def create_category(name):
+    try:
+        with connection.cursor() as cursor:
+            name = request.POST.get("name")
+            sql_query = "INSERT INTO category (id,name) VALUES (0,{})".format(name)
+            cursor.execute(sql_query)
+            cursor.commit() #-->only if update database
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':'SUCCESS','CAT_ID':id,'CODE':201})
 
+    except Exception:
+        return json.dumps({'STATUS':'ERROR','MSG':'INTERNAL ERROR'})
+
+# Delete a category
+@delete("/category/<id>")
+def delete_category(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "DELETE * FROM product WHERE category = '{}'".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            return json.dumps({'STATUS': 'SUCCESS', 'PRODUCTS': result, 'CODE': 200})
+
+    except Exception:
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+# list categories --WORKS
+@get("/categories")
+def list_category():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM category"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            return json.dumps({'STATUS': 'SUCCESS', 'CATEGORIES': result, 'CODE': 200})
+
+    except Exception:
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+# list all products --WORKS
+@get("/products")
+def list_products():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            return json.dumps({'STATUS': 'SUCCESS', 'PRODUCTS': result, 'CODE': 200})
+
+    except Exception:
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+# Getting a product--WORKS
+@get("/product/<id>")
+def get_product(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product WHERE id = '{}'".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS': 'SUCCESS', 'PRODUCTS': result, 'CODE': 200})
+
+    except Exception as e:
+        print (repr(e))
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+# List all products by category--WORKS
+@get("/category/<id>/products")
+def list_products_by_category(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product WHERE category = '{}'".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            return json.dumps({'STATUS': 'SUCCESS', 'PRODUCTS': result, 'CODE': 200})
+
+    except Exception:
+        return json.dumps({'STATUS': 'ERROR', 'MSG': 'Internal error', 'CODE': 500})
 
 @get("/")
 def index():
@@ -29,4 +122,9 @@ def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+def main():
+    run(host='localhost', port=7030)  # 127.0.0.1 if have another server already running
+
+
+if __name__ == '__main__':
+    main()
---
+++ storeDB.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE sql11189253;
+USE sql11189253;
+DROP DATABASE IF EXISTS sql11189253;
+
+CREATE TABLE category(
+id INT auto_increment PRIMARY KEY, 
+name VARCHAR(30)
+);
+
+CREATE TABLE product(
+category INT,
+description VARCHAR(50),
+price DOUBLE,
+title VARCHAR(50),
+favorite boolean default false,
+img_url VARCHAR(500),
+id INT auto_increment PRIMARY KEY
+);
+
+-- describe product;
+
+INSERT INTO category (name) VALUES 
+('Fruits'),('Vegetables'),('Meats'); -- TO ADD: (4,'Poultry'),(5,'Nuts'),(6,'Fish'),(7,'Dairy'),(8,'Health');
+
+INSERT INTO product (category, description, price, title, favorite, img_url) VALUES 
+(1, 'Fresh bananas from Northern Israel',10.50,'Bananas',false,'./images/bananas.png'),
+(1, 'Fresh strawberries locally grown',14.0,'Strawberries',false,'./images/strawberries.png'),
+(1, 'Fresh apples from Central Israel',9.0,'Apples',false,'./images/apples.png'),
+(2, 'Fresh carrots from the Golan Heights',7.5,'Carrots',false,'./images/carrots.png'),
+(2, 'Fresh tomatoes imported from Italy ',9.0,'Tomatoes',false,'./images/tomatoes.png'),
+(2, 'Fresh cucumbers from Central Israel ',6.0,'Cucumbers',false,'./images/cucumbers.png'),
+(3, 'Organic chicken',15.0,'Chicken',false,'./images/chicken.png'),
+(3, 'Farm fresh beef',19.5,'Beef',false,'./images/beef.png'),
+(3, 'Free range turkey',22.75,'Turkey',false,'./images/turkey.png');
+
+SELECT category.name, product.title
+FROM product 
+INNER JOIN category ON category.id = product.category
+GROUP BY category.id ORDER BY category.id;
+
+SELECT product.id FROM product;
+SELECT * FROM product WHERE id = 2
+
+
